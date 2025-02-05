385ed170a29cfdd0c6fa183cc2a536a56f8e5c41
Ankit Singh <ankitsingh@Ankits-MacBook-Air.local>
2025-02-05 11:55:04 +0530

    React Router, SignIn Screen and working on Signup Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import ButtonScreen from "./screens/ButtonScreen"
 import LinksScreen from "./screens/LinksScreen"
+import SigninScreen from "./screens/SigninScreen"
+import SignupScreen from "./screens/SignupScreen"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      {/* <ButtonScreen/> */}
-      <LinksScreen/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dev/button" element={<ButtonScreen/>}/>
+          <Route path="/dashboard/links" element={<LinksScreen/>}/>
+          <Route path="/auth/signin" element={<SigninScreen/>}/>
+          <Route path="/auth/signup" element={<SignupScreen/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 
